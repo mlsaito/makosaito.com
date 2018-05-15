2645897b4a025e7dd6c206ba8e4e52f4a9b4361f
Makoto Saito <mlsaito@live.com>
2018-05-15 12:05:56 +0900

    Personalize site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "That Dev in Tokyo",
+    author: "Makoto Saito",
+    description: "My personal portfolio site. I write about tech, music, and games."
   },
   pathPrefix: '/',
   plugins: [
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import avatar from '../assets/images/avatar.png'
 
 class Header extends React.Component {
     render() {
@@ -9,9 +9,10 @@ class Header extends React.Component {
             <header id="header">
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <h1><strong>Hi there! Mako here, </strong><br />
+                    I'm a software developer based in Tokyo.<br />
+                    <br/>
+                    I write about <a href="">tech</a>, <a href="">games</a>, and <a href="">music</a>.</h1>
                 </div>
                 <Footer />
             </header>
